d0683e4149f25b723069264b78e97e74ebe2a2e0
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-08-06 16:19:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Sofia'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Bulgaria'},
             ],
             'layout': {
                 'title': 'Dash Data Visualization'
